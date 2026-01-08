8cf158531de3bec9686e2c75e92a3fb7e35739f8
Dmytro <dm.oleynichenko@gmail.com>
2026-01-08 16:50:00 +0200

    Поправляет ошибки в коде

--- js/publications.js
+++ js/publications.js
@@ -1,4 +1,54 @@
-import {getNextPhotoId, getRandomInteger, getNextCommentId, generateName, generateMessage} from './util.js';
+import { generateId, getRandomInteger, getRandomIndex } from './utils.js';
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Неопознанный Енот',
+  'Артём',
+  'Олег',
+  'Крокодил',
+  'Кекс',
+  'keksobot',
+  'Снежок',
+  'Геннадий',
+  'Милана',
+  'Константин'
+];
+
+const CONSTANT_VALUES = {
+  MIN_LIKES: 15,
+  MAX_LIKES: 200,
+  MIN_COMMENTS: 0,
+  MAX_COMMENTS: 30
+};
+
+const getNextPhotoId = generateId();
+const getNextCommentId = generateId();
+
+function generateName() {
+  return NAMES[getRandomIndex(NAMES)];
+}
+
+function generateMessage() {
+  return MESSAGES[getRandomIndex(MESSAGES)];
+}
+
+function getComment() {
+  const id = getNextCommentId();
+  return {
+    id: id,
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: `${generateMessage()}`,
+    name: `${generateName()}`
+  };
+}
 
 function getComments(amount) {
   const comments = [];
@@ -10,24 +60,12 @@ function getComments(amount) {
 
 function getPublicationInfo() {
   const id = getNextPhotoId();
-
   return {
     id: id,
     url: `photos/${id}.jpg`,
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae..',
-    likes: getRandomInteger(15, 200),
-    comments: getComments(getRandomInteger(0, 30))
-  };
-}
-
-function getComment() {
-  const id = getNextCommentId();
-
-  return {
-    id: id,
-    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-    message: `${generateMessage()}`,
-    name: `${generateName()}`
+    likes: getRandomInteger(CONSTANT_VALUES.MIN_LIKES, CONSTANT_VALUES.MAX_LIKES),
+    comments: getComments(getRandomInteger(CONSTANT_VALUES.MIN_COMMENTS, CONSTANT_VALUES.MAX_COMMENTS))
   };
 }
 
@@ -39,4 +77,4 @@ function getPublications(amount) {
   return publications;
 }
 
-export {getPublications};
+export { getPublications };
