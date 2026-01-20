ddb37af6fec1e3e696e5bb7c506eb92c08e1395b
Keks <keks@htmlacademy.ru>
2026-01-20 10:56:28 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/thumbnails.js
@@ -0,0 +1,30 @@
+import { openFullMode } from './fullthumbnails.js';
+
+const container = document.querySelector('.pictures');
+const template = document.querySelector('#picture').content.querySelector('.picture');
+
+function createThumbnail(photo) {
+  const thumbnail = template.cloneNode(true);
+
+  thumbnail.querySelector('.picture__img').src = photo.url;
+  thumbnail.querySelector('.picture__likes').textContent = photo.likes;
+  thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
+
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openFullMode(photo);
+  });
+
+  return thumbnail;
+}
+
+function createThumbnails(thumbnails) {
+  const fragment = document.createDocumentFragment();
+  thumbnails.forEach((thumbnail) => {
+    fragment.appendChild(createThumbnail(thumbnail));
+  });
+  container.appendChild(fragment);
+}
+
+
+export {createThumbnails};
