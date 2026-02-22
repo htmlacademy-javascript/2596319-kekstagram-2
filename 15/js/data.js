5596c4ad2d7b15d533006d8cd27474d53a1552c9
Keks <keks@htmlacademy.ru>
2026-02-22 18:27:48 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/data.js
@@ -0,0 +1,26 @@
+const SERVER_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
+
+const Route = {
+  GET_DATA: '/data',
+  SEND_DATA: '/',
+};
+
+const Method = {
+  GET: 'GET',
+  POST: 'POST',
+};
+
+function loadData(route = Route.GET_DATA, method = Method.GET, body = null) {
+  return fetch(`${SERVER_URL}${route}`, { method, body })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`error ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+}
+
+export { loadData, Method, Route };
---
+++ 15/js/messages.js
@@ -0,0 +1,67 @@
+const SHOW_TIME = 5000;
+
+import { closePhotoEditForm } from './form.js';
+
+const successTemplate = document.querySelector('#success').content.querySelector('.success');
+const errorTemplate = document.querySelector('#error').content.querySelector('.error');
+const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+
+function closeMessage() {
+  const messageElement = getMessageElement();
+  messageElement.remove();
+  document.removeEventListener('keydown', onDocumentKeydown);
+  document.removeEventListener('click', onDocumentClick);
+}
+
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    evt.preventDefault();
+    closeMessage();
+  }
+}
+
+function onDocumentClick(evt) {
+  const messageElement = getMessageElement();
+  if (evt.target === messageElement) {
+    closeMessage();
+  }
+}
+
+function getMessageElement() {
+  return document.querySelector('.error') || document.querySelector('.success');
+}
+
+function showMessage(template, buttonClass) {
+  const messageElement = template.cloneNode(true);
+  document.body.append(messageElement);
+  const button = messageElement.querySelector(buttonClass);
+  button.addEventListener('click', () => {
+    closeMessage();
+  });
+  document.addEventListener('keydown', onDocumentKeydown);
+  document.addEventListener('click', onDocumentClick);
+}
+
+function showDataErrorMessage(template) {
+  const messageElement = template.cloneNode(true);
+  document.body.append(messageElement);
+
+  setTimeout(() => {
+    messageElement.remove();
+  }, SHOW_TIME);
+}
+
+function showSuccessMessage() {
+  showMessage(successTemplate, '.success__button');
+  closePhotoEditForm();
+}
+
+function showErrorMessage() {
+  showMessage(errorTemplate, '.error__button');
+}
+
+function showGetDataErrorMessage() {
+  showDataErrorMessage(dataErrorTemplate);
+}
+
+export {showSuccessMessage, showErrorMessage, showGetDataErrorMessage, getMessageElement };
